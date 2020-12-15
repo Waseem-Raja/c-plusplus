b67b968450c78615e9459c70cefbf88a89100fa9
Phenomenal-one <75874519+Waseem-Raja@users.noreply.github.com>
2020-12-15 20:57:02 +0530

    reverse of random size

--- reverse.cpp
+++ reverse.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+//int main()
+//{
 
   // int i;
 
@@ -16,7 +16,7 @@ int main()
          cout<<arr[i];
        }*/
    //taking number from the user
-    int arr[5],i;
+   /* int arr[5],i;
        cout<<"enter the number";
     for ( i = 0; i<5; i++)
     {
@@ -26,9 +26,33 @@ int main()
     for ( i=4; i>=0; i--)
     {
       cout<<arr[i];
-    }
-    
-    
+    }*/
 
 
+
+ int main()
+{   
+    int sizee;
+     cout<<"enter the size of the number";
+      cin>>sizee;
+      if (sizee==1)
+      {
+        cout<<"not a valid size";
+      }
+      else
+      {
+                int arr[sizee],i;
+
+           cout<<"enter the  "<<sizee<<" digit number";
+      
+          for ( i = 0; i<sizee; i++)
+           {
+               cin>>arr[i];
+           }
+                     cout<<"reverse of enterd number is  :";
+                       for ( i=sizee-1; i>=0; i--)
+                       {
+                           cout<<arr[i];
+                       }
+      }
 }
