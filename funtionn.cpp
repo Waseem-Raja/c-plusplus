6d00e09d1d5290691426f70752ca7f7d59618893
Phenomenal-one <75874519+Waseem-Raja@users.noreply.github.com>
2020-12-19 13:54:37 +0530

    function, call by value,reference

--- funtionn.cpp
+++ funtionn.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
-void swaping(int x, int y); // function decleration or prototype
- 
+
+/*void swaping(int x, int y); // function decleration or prototype
+       
 int main()
 { 
     int a, b ;
@@ -9,6 +10,8 @@ int main()
     cin>>a>>b;
     cout<<"after swapping"<<endl;
     swaping(a,b); // passing value to function(function call)here a and b are called actual arguments
+    cout<<"value of a in main:"<<a<<endl;//a=3
+    cout<<"value of b in main:"<<b<<endl;//b=4
     return 0;
 }
  void swaping(int a, int b)// function definition here a, b are called formal arguments
@@ -17,6 +20,63 @@ int main()
     temp = a;
     a= b;
     b = temp;
+    cout<<"value of a in function :"<<a<<endl;//a=4
+    cout<<"value of b in function:"<<b<<endl; //b=3
+}*/
+//changes mde in function defination to formal arguments will not effect the actual arguments 
+
+/*
+//call by reference using refrence variable &
+void swaping(int &x, int &y); // function decleration or prototype
+       
+int main()
+{ 
+    int a, b ;
+    cout<<"enter the two numbers"<<endl;
+    cin>>a>>b;
+    
+   
     cout<<"value of a :"<<a<<endl;
     cout<<"value of b :"<<b<<endl;
+     swaping(a,b); // passing value to function(function call)here a and b are called actual arguments
+    return 0;
+
+}
+ void swaping(int &a, int &b)// function definition here a, b are called formal arguments
+{
+    int temp;
+    temp = a;
+    a= b;
+    b = temp;
+    cout<<"value of a :"<<a<<endl;
+    cout<<"value of b :"<<b<<endl; 
+}*/
+// chnges mde in function definetion to formal parameters will efeect the actual arguments in the caller
+
+//using pointer variable
+void swaping(int *x, int *y); // function decleration or prototype
+       
+int main()
+{ 
+    int a, b ;
+    cout<<"enter the two numbers"<<endl;
+    cin>>a>>b;
+    
+     swaping(&a,&b); // passing value to function(function call)here a and b are called actual arguments
+     cout<<"value of a :"<<a<<endl;
+    cout<<"value of b :"<<b<<endl;
+    return 0;
+
+}
+ void swaping(int *a, int *b)// function definition here a, b are called formal arguments
+{
+    int temp;
+    temp = *a;
+    *a= *b;
+    *b = temp;
+    
+    cout<<"value of a :"<<*a<<endl;
+    cout<<"value of b :"<<*b<<endl;
+   
+     
 }
