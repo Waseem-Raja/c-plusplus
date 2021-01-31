a2f6233f6942362fb825749ea590c98e80879ade
Phenomenal-one <75874519+Waseem-Raja@users.noreply.github.com>
2021-01-31 13:47:42 +0530

    unionss in cpp

--- unionn.cpp
+++ unionn.cpp
@@ -1,4 +1,5 @@
 
+#include<string.h>
 #include<iostream>
 using namespace std;
 
@@ -6,20 +7,41 @@ union  student
 {
          // string  name;  we canot use strings with union
    
-   char rollno;
-   double r;  //size of union is decided by the size of largest member of union
+   int rollno;
+   int phn;
+   char r[20];  //size of union is decided by the size of largest member of union
      };
 
 int main()
 {
     student s1,s2;
-    /*s1.rollno=30;
-    s2.rollno=39;
-    
+    /*
+    s1.rollno=6;
+    s1.phn=77777;
+    strcpy(s1.r,"raja");
+
+    cout<<s1.phn<<endl;
+    cout<<s1.r<<endl;   //here only raja will be printed which is assigned at the last to memoray location
+    cout<<s1.rollno;*/
+     
+
+     //way 2
+   // here all values will be printed cox every value is printed when they are used
+     
+    s1.rollno=6;
     cout<<s1.rollno<<endl;
-    cout<<s2.rollno<<endl;*/
-    cout<<sizeof(s1)<<endl;  // 8 bytes 
-    cout<<sizeof(s2)<<endl;
+    s1.phn=77777;
+    cout<<s1.phn<<endl;
+    strcpy(s1.r,"raja");
+      cout<<s1.r<<endl;  
+    
+
+
+
+
+
+   // cout<<sizeof(s1)<<endl;  // 8 bytes 
+    //cout<<sizeof(s2)<<endl;
 
 
 
