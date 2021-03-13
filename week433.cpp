1186bfa010fa9574ee616a7a87c018261bcf6295
Phenomenal-one <75874519+Waseem-Raja@users.noreply.github.com>
2021-03-13 10:56:45 +0530

    REMOVE DUPLICATES FROM AN SORTED ARRAY

---
+++ week433.cpp
@@ -0,0 +1,26 @@
+//Remove duplicates in sorted array
+#include <iostream>
+using namespace std;
+
+/*int main()
+{
+    int arr[] = {10, 10, 10, 30, 40, 40, 50, 80, 80, 100};
+
+    int i, j;
+    for (i = 0; i <= 9; i++)  
+    {
+
+        for (j = i + 1; j <= 9; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                arr[j] = 0;
+            }
+        }
+    }
+    for (i = 0; i <= 9; i++)
+    {
+        if (arr[i] > 0)
+            cout << arr[i] << endl;
+    }
+}*/
